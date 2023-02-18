cda814f45a99d1cccf911edd1d5a22800cef0e27
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2023-02-18 09:06:08 -0700

    Fixing some python things in main branch.

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,4 +1,11 @@
+# Adding default Python for Mac
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "I'm a Python Darwin"
+  export PATH="/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Versions/3.8/bin:${PATH}"
+fi
 # Setting some python variables
-export PYTHON3=`which python3`
 # This assumes an anaconda distribution
+export PYTHON3=`which python3`
 export PYTHON3HOME="${PYTHON3%/*/*}"
+export pythonthreedll="$PYTHON3HOME/lib"
+export LD_LIBRARY_PATH=$PYTHON3HOME/lib:$LD_LIBRARY_PATH
