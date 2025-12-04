280355735b5beaae8df9a92fb2b14849395314e1
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2025-12-04 06:49:01 -0700

    Updating some python stuff.

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,4 +1,8 @@
-# Setting some python variables
+## Setting some python variables
 export PYTHON3=`which python3`
-# This assumes an anaconda distribution
+##
+## This assumes an anaconda distribution
 export PYTHON3HOME="${PYTHON3%/*/*}"
+export pythonthreedll="$PYTHON3HOME/lib"
+export LD_LIBRARY_PATH=$PYTHON3HOME/lib:$LD_LIBRARY_PATH
+
